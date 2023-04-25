047df36bf4bd9099afd47cc625eeb3239997df42
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-04-26 00:32:52 +0400

    create Student.cs file

---
+++ EncapsulationTask/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace EncapsulationTask.Models;
+
+internal class Student
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Student()
+    {
+        
+    }
+    public Student(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+}
