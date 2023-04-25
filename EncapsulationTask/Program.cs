7ee0051d84ccf87412c3236d85c9f19dd9338e63
MrMukhtarov <101628108+MrMukhtarov@users.noreply.github.com>
2023-04-26 00:36:46 +0400

    create encapsulation flder

---
+++ EncapsulationTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using EncapsulationTask.Models;
+
+Student st1 = new Student("Nicat","Muxtarov");
+Student st2 = new Student("Nicat","Muxtarov");
+Student st3 = new Student("Nicat","Muxtarov");
+Student st4 = new Student("Nicat","Muxtarov");
+Student[] stuArr = new Student[4];
+stuArr[0] = st1;
+stuArr[1] = st2;
+stuArr[2] = st3;
+stuArr[3] = st4;
+Group gr1 = new Group(137,5,stuArr);
+Console.WriteLine(gr1.AddStudent(st1));
+gr1.GetStudents();
